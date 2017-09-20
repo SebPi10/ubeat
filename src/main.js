76b8774691dd43474e7dd91c42c957aa1e38b06e
lcdz1995 <looch_skateboy@msn.com>
2017-09-20 18:00:29 -0400

    Suppression de bootstrap et jquery (js seulement)

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import bootstrapCSS from './assets/bootstrap/css/bootstrap.min.css';
-import bootstrapJS from './assets/bootstrap/js/bootstrap.min';
-import jquery from './assets/jquery/jquery.min';
+// import bootstrapJS from './assets/bootstrap/js/bootstrap.min';
+// import jquery from './assets/jquery/jquery.min';
 
 Vue.config.productionTip = false;
 Vue.use(bootstrapCSS);
-Vue.use(jquery);
-Vue.use(bootstrapJS);
+// Vue.use(jquery);
+// Vue.use(bootstrapJS);
 
 /* eslint-disable no-new */
 new Vue({
