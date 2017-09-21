33479526f170d976edb02ef0dafeff380ac0f951
olivier <olivier.parent.6@ulaval.ca>
2017-09-20 20:31:50 -0400

    Création de template de base avec son css - Il manque encore le javascript à faire plus tard

--- src/App.vue
+++ src/App.vue
@@ -17,4 +17,46 @@
 </script>
 
 <style>
+  /** Body **/
+  body {
+    padding: 0;
+    background-color: orange;
+  }
+
+  /** Footer **/
+  #footer {
+    bottom: 0;
+    width: 100%;
+    position: fixed;
+    background-color: deeppink;
+  }
+
+  #footer #music_zone {
+    display: flex;
+  }
+
+  #footer #music_zone #music_player_zone {
+    width: 60%;
+    height: 100px;
+    align-self: flex-start;
+    background-color: blue;
+  }
+
+  #footer #music_zone #music_info_zone {
+    width: 40%;
+    height: 100px;
+    align-self: flex-end;
+    background-color: chartreuse;
+  }
+
+  #footer #copyright_zone {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  #footer #copyright_zone p {
+    margin: 2px 0 2px 0;
+    color: yellow
+  }
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,43 +1,111 @@
 <template>
-  <nav>
-    <!--<ul>
-      <li>
-        <router-link to="/">Home</router-link>
-      </li>
-      <li>
-        <router-link to="/album">Album</router-link>
-      </li>
-      <li>
-        <router-link to="/artist">Artist</router-link>
-      </li>
-    </ul>-->
-    <ul class="nav">
-      <li class="nav-item">
-        <router-link to="/"><img src="../assets/img/favicon.png" height="50" width="50" alt="UBeat" title="Ubeat"></router-link>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Playlists</a>
-      </li>
-      <li>
-        <div id="navSearchDropdown">
-          <input type="text" placeholder="Search..." id="searchInput" onkeyup="filterNavSearch();">
-          <button onclick="clearNavSearchInput();" id="resetSearchInputBtn">X</button>
-          <div id="navSearchList">
+  <header id="header">
+    <nav id="principal_nav">
+      <ul class="nav nav-left">
+        <li class="nav-item">
+          <router-link to="/"><img src="../assets/img/logo.png" height="50" width="100" alt="UBeat" title="Ubeat">
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link nav-eleme" href="#"><span>Playlists</span></a>
+        </li>
+      </ul>
+      <ul class="nav nav-right">
+        <li class="nav-item">
+          <div class="input-group navSearchDropdown">
+            <span class="input-group-addon fa fa-search">S</span>
+            <input id="navInputSearch" type="text" class="form-control" placeholder="Search..." aria-label="Search..."
+                   onkeyup="filterNavSearch();" onfocus="onFocusInputSearch();">
+            <span class="input-group-btn">
+              <button id="resetSearchInputBtn" class="btn btn-secondary" type="button" onclick="clearNavSearchInput();">X</button>
+            </span>
           </div>
-        </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-           aria-expanded="false">Username</a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="#">Profile</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Logout</a>
-        </div>
-      </li>
-    </ul>
-  </nav>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link nav-eleme dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
+             aria-expanded="false"><span>Username</span></a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#"><span>Profile</span></a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#"><span>Logout</span></a>
+          </div>
+        </li>
+      </ul>
+    </nav>
+    <div id="navSearchList">
+    </div>
+  </header>
 </template>
 
 <style>
+  /** Header **/
+  #header {
+    height: 50px;
+    width: 100%;
+  }
+
+  #principal_nav {
+    top: 0;
+    position: fixed;
+    height: 50px;
+    width: 100%;
+    display: flex;
+    background-color: purple;
+  }
+
+  #principal_nav ul.nav-left {
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  #principal_nav ul.nav-right {
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+
+  #principal_nav ul.nav li.nav-item {
+    min-width: 100px;
+    text-align: center;
+  }
+
+  #principal_nav .nav > li > a {
+    height: 50px;
+    padding: 0;
+  }
+
+  #principal_nav .nav > li > a.nav-eleme {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+  }
+
+  #principal_nav .nav > li > a.nav-eleme:hover{
+    background-color: forestgreen;
+    color: purple;
+  }
+
+  #principal_nav .nav > li > a.nav-eleme:focus{
+    background-color: yellow;
+    color: #8a6d3b;
+  }
+
+  #principal_nav ul.nav li.nav-item .navSearchDropdown {
+    width: 175px;
+  }
+
+  #navSearchList {
+    right: 100px;
+    top: 50px;
+    position: fixed;
+    height: 50vh;
+    width: 50vw;
+    background-color: darkred;
+    display:none;
+    visibility: hidden;
+  }
 </style>
