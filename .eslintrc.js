0196ad7a937991cbe5a3d238e8fa9856dfb4db36
Luc <luc.geffrault@insa-rennes.fr>
2017-09-22 02:23:41 +0200

    Enlevé la règle 'no-console' pour console.log

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,5 +38,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     // linebreak-style
     'linebreak-style': 0//["error", "windows"]
+    'no-new': 0,
+    'no-console': 0
   }
 };
