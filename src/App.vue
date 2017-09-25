efea5750f8e7efaf061d20c90046d7ea036a6bdf
olivier <olivier.parent.6@ulaval.ca>
2017-09-25 16:44:21 -0400

    Ajout d'un menu sur le côté et ajustement du CSS - Le menu n'est pas terminé, il manque à le modifier et à ajuster le tout pour utiliser du javascript (ne pas y toucher :D)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="side_menu_open">
     <nav-menu></nav-menu>
     <router-view></router-view>
   </div>
@@ -23,9 +23,29 @@
     background-color: #e6e6e6;
   }
 
+  #app {
+    height: 80vh;
+  }
+
+  /**Side menu**/
+  .side_menu_open #side_menu {
+    width: 20vw; /*250px*/
+  }
+
+  .side_menu_open .main_container {
+    margin-left: 20vw; /*250px*/
+  }
+
+  .main_container {
+    height: 72vh;
+    transition: margin-left .5s;
+    padding: 20px;
+  }
+
   /** Footer **/
   #footer {
     bottom: 0;
+    height: 20vh;
     width: 100%;
     position: fixed;
     background-color: #e6e6e6;
@@ -37,7 +57,7 @@
 
   #footer #music_zone #music_player_zone {
     width: 60%;
-    height: 100px;
+    height: 16vh; /*100px*/
     align-self: flex-start;
     background-color: #383838;
     border: #20B2AA solid 1px;
@@ -46,7 +66,7 @@
 
   #footer #music_zone #music_info_zone {
     width: 40%;
-    height: 100px;
+    height: 16vh; /*100px*/
     align-self: flex-end;
     background-color: #383838;
     border: #20B2AA solid 1px;
@@ -57,6 +77,7 @@
     display: flex;
     align-items: center;
     justify-content: center;
+    height: 4vh;
   }
 
   #footer #copyright_zone p {
